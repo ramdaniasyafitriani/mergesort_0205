b03719c9ffcf0f38dd0265e2140952595f3a9a2e
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-04-15 11:34:21 +0800

    membuat prosedur input

--- mergesort.cpp
+++ mergesort.cpp
@@ -5,3 +5,33 @@ using namespace std;
 int arr[20], B[20];
 // n is array input size
 int n;
+
+#include<iostream>
+using namespace std;
+
+// Create main and temporary array
+int arr[20], B[20];
+int n;
+
+void input() {
+    while (true) 
+    {
+        cout << "masukkan panjang element array : ";
+        cin >> n;
+
+        if (n <= 20) 
+        {
+            break;
+        }
+        else 
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n--------------------------" << endl;
+    cout << "\nInputkan isi element array" << endl;
+    cout << "\n--------------------------" << endl;
+
+
+    
+}
