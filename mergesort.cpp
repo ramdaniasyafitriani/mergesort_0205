cd24ba0a87d74c67ec56eb23a56bb696a38e3e4c
ramdaniasyafitriani <ramdaniania04@gmail.com>
2025-04-15 12:04:42 +0800

    membuat prosedur output

--- mergesort.cpp
+++ mergesort.cpp
@@ -88,7 +88,20 @@ void mergeSort(int low, int high)
         k++; // step 4.f.iii
     }
 
+    // step 5
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
+}
 
-
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort) : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
